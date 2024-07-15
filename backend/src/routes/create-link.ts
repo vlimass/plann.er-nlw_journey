2229e20f522f36e53d82c88e0af86e7fa74528af
Vinícius Lima <villylss@gmail.com>
2024-07-14 22:11:37 -0300

    feature: segunda aula backend nodejs concluída

---
+++ backend/src/routes/create-link.ts
@@ -0,0 +1,39 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from 'zod';
+import { prisma } from "../lib/prisma";
+
+export async function createLink(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post('/trips/:tripId/links', {
+    schema: {
+      params: z.object({
+        tripId: z.string().uuid(),
+      }),
+      body: z.object({
+        title: z.string().min(4), 
+        url: z.string().url(),
+      })
+    },
+  }, async (request) => {
+    const { tripId } = request.params
+    const { title, url } = request.body
+
+    const trip = await prisma.trip.findUnique({
+      where: { id: tripId }
+    })
+
+    if (!trip) {
+      throw new Error('Trip not found.')
+    }
+
+    const link = await prisma.link.create({
+      data: {
+        trip_id: tripId,
+        title, 
+        url,
+      }
+    })
+
+    return { linkId: link.id }
+  })
+}
